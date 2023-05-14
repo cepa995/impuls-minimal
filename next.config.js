8b4a1961561529690a326eb3bf1e991c329c2080
cepa995 <stefan.ai.research@gmail.com>
2023-05-14 23:07:18 +0200

    aaa

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/sections/1",
+        destination: "/sections/isidora-pokrajac-cubeus",
         permanent: false,
       },
     ]
