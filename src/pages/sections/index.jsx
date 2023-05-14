2bbfa3aa43b682b5af2ff5ff19c62b8f8b5d3c11
cepa995 <stefan.ai.research@gmail.com>
2023-05-14 19:24:32 +0200

    Adding Sections parent component

--- src/pages/sections/index.jsx
+++ src/pages/sections/index.jsx
@@ -1,7 +1,7 @@
 import { Wrapper } from "@layout/index";
 import SEO from "@components/seo";
 
-export default function Program() {
+export default function Section() {
   return (
     <Wrapper>
       <SEO pageTitle={'Šetnja'} />
